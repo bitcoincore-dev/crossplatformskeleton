5a586d4d2d20c9bfef2329cd7c9706ebd14bc689
randymcmillan <randy.lee.mcmillan@gmail.com>
2020-12-15 14:37:37 -0500

    fix some qt config issues

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -2,8 +2,8 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#include <mainwindow.h>
-#include <forms/ui_mainwindow.h>
+#include "mainwindow.h"
+#include "forms/ui_mainwindow.h"
 //#include <qt/overviewpage.moc>
 
 MainWindow::MainWindow(QMainWindow *parent) :
